9ddac24e107f6da2025ed64c0712f09c38c3a56b
Nishant2356 <nm2356opp@gmail.com>
2026-04-06 23:06:09 +0530

    deployment-test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Settings2, Type, Sparkles, PaintBucket, Palette, SlidersHorizontal, MousePointerClick } from 'lucide-react';
 
 export default function App() {
